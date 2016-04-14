0f92c06ab5d215be56ad745914407738ce75f1ae
Jordy Hulck <jordy.hulck@outlook.com>
2016-04-15 00:50:27 +0200

    Make GTA API x86 only

--- GTAScriptHook/AssemblyInfo.cpp
+++ GTAScriptHook/AssemblyInfo.cpp
@@ -32,7 +32,7 @@ using namespace System::Security::Permissions;
 // by using the '*' as shown below:
 
 // ##GENVERSION
-[assembly:AssemblyVersion("2016.03.29.1124")];
+[assembly:AssemblyVersion("2016.04.15.0049")];
 // #/GENVERSION
 //
 
